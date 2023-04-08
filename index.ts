39d10a3ef7913d4ccf64de31739b740dbd598823
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2023-04-08 23:17:36 +0900

    add test script

---
+++ index.ts
@@ -0,0 +1,18 @@
+import {SerialPort} from "serialport";
+
+const sp = new SerialPort({
+  path: "/dev/serial0",
+  baudRate: 9600
+})
+
+sp.on("close", (err) => {
+  console.error(err)
+
+  process.exit(1)
+})
+
+sp.on("data", (data) => {
+  console.log(data)
+})
+
+sp.write([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00])
