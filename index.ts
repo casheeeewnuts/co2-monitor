892d8600cf5a4108fb7f355e0aa2d3e00d65b299
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2023-04-09 00:13:53 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import {SerialPort} from "serialport";
 
+const READ_CO2 = [0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00];
+
 const sp = new SerialPort({
   path: "/dev/serial0",
   baudRate: 9600
@@ -15,4 +17,17 @@ sp.on("data", (data: Buffer) => {
   console.log(data)
 })
 
-sp.write([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00])
+sp.write([...READ_CO2, getCheckSum(READ_CO2)])
+
+function getCheckSum(packet: number[]): number {
+  const [startByte, ...bytes] = packet;
+  let checksum = 0;
+
+  for (let i = 0; i < 7; i++) {
+    checksum += bytes[i];
+  }
+
+  checksum = 0xFF - checksum;
+
+  return checksum + 0x01;
+}
